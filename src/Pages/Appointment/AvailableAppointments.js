f41189093239606423fd720cceebfe417da6f21f
Somen Singha <somensingha06@gmail.com>
2022-05-16 22:42:10 +0600

    added

--- src/Pages/Appointment/AvailableAppointments.js
+++ src/Pages/Appointment/AvailableAppointments.js
@@ -7,14 +7,14 @@ const AvailableAppointments = ({ date }) => {
   const [services, setServices] = useState([]);
   const [treatment, setTreatment] = useState(null);
   useEffect(() => {
-    fetch("services.json")
+    fetch("http://localhost:5000/service")
       .then((res) => res.json())
       .then((data) => setServices(data));
   }, []);
   return (
     <div>
       <h4 className="text-xl text-primary text-center">
-        Available Appointments on Available Appointment on {format(date, "pp")}
+        Available Appointments on {format(date, "PP")}
       </h4>
       <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-5">
         {services.map((service) => (
@@ -25,7 +25,13 @@ const AvailableAppointments = ({ date }) => {
           ></Service>
         ))}
       </div>
-      {treatment && <BookingModal treatment={treatment}></BookingModal>}
+      {treatment && (
+        <BookingModal
+          date={date}
+          treatment={treatment}
+          setTreatment={setTreatment}
+        ></BookingModal>
+      )}
     </div>
   );
 };
--- src/Pages/Home/Info.js
+++ src/Pages/Home/Info.js
@@ -9,17 +9,17 @@ const Info = () => {
     <div className="grid grid-cols-1 lg:grid-cols-3 gap-5">
       <InfoCard
         cardTitle="Opening Hours"
-        bgClass="bg-gradient-to-r from-secondary to-primary"
+        bgclassName="bg-gradient-to-r from-secondary to-primary"
         img={clock}
       ></InfoCard>
       <InfoCard
         cardTitle="Our locations"
-        bgClass="bg-neutral"
+        bgclassName="bg-neutral"
         img={marker}
       ></InfoCard>
       <InfoCard
         cardTitle="Contact us"
-        bgClass="bg-gradient-to-r from-secondary to-primary"
+        bgclassName="bg-gradient-to-r from-secondary to-primary"
         img={phone}
       ></InfoCard>
     </div>
